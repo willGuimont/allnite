dae1c1f371b62deee3a68cf19f89ab57eef7a1ff
William Guimont-Martin <william.guimont-martin.1@ulaval.ca>
2020-07-30 14:33:57 -0400

    string format

--- src/allnite.nim
+++ src/allnite.nim
@@ -2,6 +2,7 @@ import lists
 import os
 import json
 import strutils
+import parseopt2
 
 
 type
@@ -17,11 +18,12 @@ type
         logDir : string
         runs    : seq[Execution]
 
+
 when isMainModule:
     import parseopt
 
     if paramCount() != 1:
-        echo "Expected: allnite path/run/config"
+        echo "Expected: allnite <path/run/config>"
         quit(1)
     
     let fileContent = readFile(paramStr(1))
@@ -33,16 +35,16 @@ when isMainModule:
     createDir(runs.logDir)
 
     for i, run in runs.runs:
-        echo "[", i + 1, "/", len(runs.runs), "] Running ", run.name, "..."
+        echo "[$1/$1] Running $3..." % [$(i + 1), $len(runs.runs), run.name]
         var cmd = run.executable;
 
         for param in run.parameters:
             if param.name != "":
-                cmd = cmd & " --" & param.name
+                cmd = cmd & " --$1" % [param.name] 
             cmd = cmd & " " & param.value
         
         if run.redirectLog:
-            cmd = "(" & cmd & ")" & " > " & runs.logDir & "/" & run.name
+            cmd = "($1) > $2/$3" % [cmd, runs.logDir, run.name]
 
         echo "\tExecuting: ", cmd
         discard execShellCmd(cmd)
